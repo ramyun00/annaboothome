1d7fd1d3773a55a7fdf8e2f121e55e02c3913d37
Anna Boot <anna.boot@ibm.com>
2023-05-18 15:03:13 -0400

    Squash it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBXixL5GFDJli04OAIs3-bTYO3ghcXgOzw",
+  authDomain: "annaboothome.firebaseapp.com",
+  projectId: "annaboothome",
+  storageBucket: "annaboothome.appspot.com",
+  messagingSenderId: "1016584318334",
+  appId: "1:1016584318334:web:c4c6078197acd07e11d72f",
+  measurementId: "G-R1SXX7VQDX"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Test deploy
     </div>
   );
 }
