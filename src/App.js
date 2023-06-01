d58b48e379b2958d047feeb566b0cc588130789e
Anna Boot <anna.boot@ibm.com>
2023-06-01 13:27:19 -0400

    Make some notes about what I'm doing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -26,16 +26,28 @@ const firebaseConfig = {
 // const analytics = getAnalytics(app);
 
 function App() {
+  const [apiResponse, setApiResponse] = useState('');
   useEffect(() => {
     const call = fetch("https://annaboothome.ue.r.appspot.com/")
-    .then(res => res.text());
-  console.log('call', call);
+    .then(stream => stream.text())
+    .then(result => setApiResponse(result));
   }, [])
   
   return (
     <div className="app">
       <Title title="Cloud Deployment Test"></Title>
       Version {process.env.REACT_APP_VERSION}
+      <p>
+        <ul>
+          <li>Containerized UI one time lol</li>
+          <li>Get response: {apiResponse}</li>
+          <li>Enabled CORS</li>
+          <li>
+            <a href="https://developer.mozilla.org/en-US/docs/Web/API/Streams_API#concepts_and_usage" target="_BLANK" rel="noreferrer">What is a readableStream?</a>
+          </li>
+          <li>I left some Compute Engine instances running by accident and it cost me $2.50 :(</li>
+        </ul>
+      </p>
     </div>
   );
 }
